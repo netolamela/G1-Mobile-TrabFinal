2ba72e6e0a0425bc3ce8360c9130c8295ef777b4
ecard58 <emanuelcardoso58@gmail.com>
2023-11-23 21:31:11 -0300

    fix visual cadastro

--- suricatoSortudo/src/Pages/Cadastro/index.js
+++ suricatoSortudo/src/Pages/Cadastro/index.js
@@ -1,12 +1,30 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity, TextInput, Modal } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import { useNavigation } from '@react-navigation/native';
-import axios from 'axios';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  TextInput,
+  Modal,
+} from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { useNavigation } from "@react-navigation/native";
+import axios from "axios";
 
 const InputComIcone = ({ icone, ...props }) => {
   return (
-    <View style={[styles.containerInput, { backgroundColor: 'white', marginBottom: 16, width: 300, alignSelf: 'center' }]}>
+    <View
+      style={[
+        styles.containerInput,
+        {
+          backgroundColor: "white",
+          marginBottom: 16,
+          width: 280,
+          alignSelf: "center",
+        },
+      ]}
+    >
       <Icon name={icone} size={20} color="#808080" style={styles.icone} />
       <TextInput {...props} style={styles.input} />
     </View>
@@ -14,34 +32,36 @@ const InputComIcone = ({ icone, ...props }) => {
 };
 
 export default function Cadastro() {
-
   const navigation = useNavigation();
-  const [usuario, setUsuario] = useState('');
-  const [senha, setSenha] = useState('');
-  const [confirmaSenha, setConfirmaSenha] = useState('');
+  const [usuario, setUsuario] = useState("");
+  const [senha, setSenha] = useState("");
+  const [confirmaSenha, setConfirmaSenha] = useState("");
   const [isModalVisible, setIsModalVisible] = useState(false);
-  const [mensagemErro, setMensagemErro] = useState('');
+  const [mensagemErro, setMensagemErro] = useState("");
 
   const lidarComPressionarCadastro = async () => {
     if (!usuario || !senha || !confirmaSenha) {
-      setMensagemErro('Preencha todos os campos para cadastrar.');
+      setMensagemErro("Preencha todos os campos para cadastrar.");
     } else if (senha !== confirmaSenha) {
-      setMensagemErro('As senhas não coincidem. Tente novamente.');
+      setMensagemErro("As senhas não coincidem. Tente novamente.");
     } else {
       const usuarioExiste = await verificarUsuarioExiste(usuario);
 
       if (usuarioExiste) {
-        setMensagemErro('Este usuário já existe. Tente outro.');
+        setMensagemErro("Este usuário já existe. Tente outro.");
       } else {
         try {
-          const resposta = await axios.post('https://655ac5066981238d054db51e.mockapi.io/login/usuarios', {
-            user: usuario,
-            senha: senha,
-            grupo: 'funcionario',
-          });
+          const resposta = await axios.post(
+            "https://655ac5066981238d054db51e.mockapi.io/login/usuarios",
+            {
+              user: usuario,
+              senha: senha,
+              grupo: "funcionario",
+            }
+          );
           setIsModalVisible(true);
         } catch (erro) {
-          setMensagemErro('Falha ao cadastrar. Por favor, tente novamente.');
+          setMensagemErro("Falha ao cadastrar. Por favor, tente novamente.");
         }
       }
     }
@@ -49,33 +69,61 @@ export default function Cadastro() {
 
   const fecharModal = () => {
     setIsModalVisible(false);
-    setMensagemErro('');
-    navigation.navigate('Home');
+    setMensagemErro("");
+    navigation.navigate("Home");
   };
 
   const verificarUsuarioExiste = async (nomeUsuario) => {
     try {
-      const resposta = await axios.get(`https://655ac5066981238d054db51e.mockapi.io/login/usuarios?user=${nomeUsuario}`);
+      const resposta = await axios.get(
+        `https://655ac5066981238d054db51e.mockapi.io/login/usuarios?user=${nomeUsuario}`
+      );
       return resposta.data.length > 0;
     } catch (erro) {
-      console.log('Erro ao verificar usuário:', erro);
+      console.log("Erro ao verificar usuário:", erro);
       return false;
     }
   };
 
   return (
     <View style={styles.container}>
-      <Image source={require('../../assets/fundo.png')} style={styles.imagemFundo} />
-      <Image source={require('../../assets/logo.png')} style={styles.logo} />
-      <TouchableOpacity onPress={() => navigation.navigate('Home')} style={styles.botaoVoltar}>
+      <Image
+        source={require("../../assets/fundo.png")}
+        style={styles.imagemFundo}
+      />
+      <Image source={require("../../assets/logo.png")} style={styles.logo} />
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Home")}
+        style={styles.botaoVoltar}
+      >
         <Icon name="arrow-left" size={25} color="white" />
       </TouchableOpacity>
       <View style={styles.cadastro}>
         <View style={[styles.campos, { marginTop: 10 }]}>
-          <InputComIcone icone="user" placeholder="Digite seu usuário" placeholderTextColor="#808080" onChangeText={setUsuario} />
-          <InputComIcone icone="lock" placeholder="Digite sua senha" secureTextEntry placeholderTextColor="#808080" onChangeText={setSenha} />
-          <InputComIcone icone="lock" placeholder="Confirme sua senha" secureTextEntry placeholderTextColor="#808080" onChangeText={setConfirmaSenha} />
-          <TouchableOpacity style={styles.botaoCadastro} onPress={lidarComPressionarCadastro}>
+          <InputComIcone
+            icone="user"
+            placeholder="Digite seu usuário"
+            placeholderTextColor="#808080"
+            onChangeText={setUsuario}
+          />
+          <InputComIcone
+            icone="lock"
+            placeholder="Digite sua senha"
+            secureTextEntry
+            placeholderTextColor="#808080"
+            onChangeText={setSenha}
+          />
+          <InputComIcone
+            icone="lock"
+            placeholder="Confirme sua senha"
+            secureTextEntry
+            placeholderTextColor="#808080"
+            onChangeText={setConfirmaSenha}
+          />
+          <TouchableOpacity
+            style={styles.botaoCadastro}
+            onPress={lidarComPressionarCadastro}
+          >
             <Text style={styles.textoBotao}>Cadastrar</Text>
           </TouchableOpacity>
           {mensagemErro ? (
@@ -87,7 +135,9 @@ export default function Cadastro() {
       <Modal visible={isModalVisible} transparent animationType="slide">
         <View style={styles.containerModal}>
           <View style={styles.conteudoModal}>
-            <Text style={styles.textoModal}>Você cadastrou uma nova conta!</Text>
+            <Text style={styles.textoModal}>
+              Você cadastrou uma nova conta!
+            </Text>
             <TouchableOpacity onPress={fecharModal} style={styles.botaoFechar}>
               <Text style={styles.textoBotaoFechar}>Retorne para Home</Text>
             </TouchableOpacity>
@@ -96,33 +146,33 @@ export default function Cadastro() {
       </Modal>
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#BD7834',
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "#BD7834",
+    justifyContent: "center",
+    alignItems: "center",
   },
   imagemFundo: {
-    position: 'absolute',
+    position: "absolute",
     top: 0,
     left: 0,
-    width: '100%',
-    height: '100%',
-    resizeMode: 'cover',
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover",
   },
   logo: {
     width: 150,
     height: 150,
-    resizeMode: 'contain',
+    resizeMode: "contain",
     borderRadius: 10,
-    shadowColor: 'black',
+    shadowColor: "black",
     shadowOffset: { width: 0, height: 2 },
   },
   botaoVoltar: {
-    position: 'absolute',
+    position: "absolute",
     top: 20,
     left: 20,
     padding: 10,
@@ -131,10 +181,10 @@ const styles = StyleSheet.create({
     marginTop: 50,
   },
   containerInput: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     borderWidth: 1,
-    borderColor: '#808080',
+    borderColor: "#808080",
     borderRadius: 8,
     paddingHorizontal: 10,
     marginBottom: 16,
@@ -148,10 +198,10 @@ const styles = StyleSheet.create({
     width: 200,
   },
   cadastro: {
-    width: '80%',
+    width: "80%",
     borderRadius: 25,
-    backgroundColor: '#D4BF6A',
-    shadowColor: 'black',
+    backgroundColor: "#D4BF6A",
+    shadowColor: "black",
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 1,
     shadowRadius: 4,
@@ -159,45 +209,45 @@ const styles = StyleSheet.create({
     padding: 20,
   },
   botaoCadastro: {
-    backgroundColor: 'green',
+    backgroundColor: "green",
     padding: 10,
     borderRadius: 100,
     marginTop: 25,
-    width: '100%',
-    alignItems: 'center',
+    width: "100%",
+    alignItems: "center",
   },
   textoBotao: {
-    color: 'white',
+    color: "white",
     fontSize: 18,
   },
   mensagemErro: {
-    color: 'red',
+    color: "red",
     marginTop: 10,
-    textAlign: 'center',
+    textAlign: "center",
   },
   containerModal: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
   },
   conteudoModal: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     padding: 20,
     borderRadius: 10,
-    alignItems: 'center',
+    alignItems: "center",
   },
   textoModal: {
     fontSize: 18,
     marginBottom: 20,
   },
   botaoFechar: {
-    backgroundColor: 'green',
+    backgroundColor: "green",
     padding: 10,
     borderRadius: 5,
   },
   textoBotaoFechar: {
-    color: 'white',
+    color: "white",
     fontSize: 16,
   },
 });
