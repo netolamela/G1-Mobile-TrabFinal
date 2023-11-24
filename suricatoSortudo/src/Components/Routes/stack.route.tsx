108a5dbbcdc278707a21e00643e4eaa17229b45a
LucasGonRo <gonzagal206@gmail.com>
2023-11-24 16:26:55 -0300

    splash screen

--- suricatoSortudo/src/Components/Routes/stack.route.tsx
+++ suricatoSortudo/src/Components/Routes/stack.route.tsx
@@ -1,17 +1,42 @@
-import React from "react";
-
+import React, { useEffect, useState } from 'react';
+import { View, Text , Image} from 'react-native';
+import NetInfo from '@react-native-community/netinfo';
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import DrawerNavigate from "./drawer.routes";
 import Login from "../../Pages/Login";
+import Animacao from "../TelaScreen";
 
 const Stack = createNativeStackNavigator();
 
 const StackNavigate = () => {
+  const [isConnected, setIsConnected] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = NetInfo.addEventListener(state => {
+      setIsConnected(state.isConnected);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Login" component={Login} />
-      <Stack.Screen name="Drawer" component={DrawerNavigate} />
-    </Stack.Navigator>
+    <View style={{ flex: 1 }}>
+      {isConnected ? (
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Animacao" component={Animacao} />
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Drawer" component={DrawerNavigate} />
+        </Stack.Navigator>
+      ) : (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Text style={{ color: 'red', fontSize: 18 }}>
+          
+          </Text>
+        </View>
+      )}
+    </View>
   );
 };
 
